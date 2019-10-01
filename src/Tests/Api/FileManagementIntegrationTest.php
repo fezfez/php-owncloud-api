379d09676212272998dcbba38333583ed32d27df
stephane <sdemonchaux@cube43.fr>
2019-10-01 09:27:47 +0200

    Update and modernize

--- src/Tests/Api/FileManagementIntegrationTest.php
+++ src/Tests/Api/FileManagementIntegrationTest.php
@@ -4,70 +4,63 @@ declare(strict_types=1);
 
 namespace Owncloud\Tests\Api;
 
-use Owncloud\Api;
+use Owncloud\Api\FileManagement;
 use PHPUnit\Framework\TestCase;
 use function getenv;
 use function rand;
 
 /**
- * @coversDefaultClass Api\FileManagement
+ * @coversDefaultClass FileManagement
  */
 class FileManagementIntegrationTest extends TestCase
 {
-    /** @var Api  */
+    /** @var FileManagement  */
     protected $api;
 
     public function setUp() : void
     {
-        if (getenv('owncloud_host') !== '') {
-            $this->api = new Api(getenv('owncloud_host'), getenv('owncloud_user'), getenv('owncloud_password'));
-        } else {
-            $this->api = new Api($_SERVER['owncloud_host'], $_SERVER['owncloud_user'], $_SERVER['owncloud_password']);
-        }
+        $this->api = new FileManagement($_SERVER['owncloud_host'], $_SERVER['owncloud_user'], $_SERVER['owncloud_password']);
     }
 
     /**
      * @group internet
-     * @covers                   Api\FileManagement::update
+     * @covers                   FileManagement::update
      */
     public function testRead() : void
     {
         $filename = $this->getTestFilename();
         $expected = 'Example random number :' . rand(0, 15000);
 
-        $this->api->fileManagement()->update($filename, $expected);
-        $actual = $this->api->fileManagement()->read($filename);
+        $this->api->update($filename, $expected);
 
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $this->api->read($filename));
     }
 
     /**
      * @group internet
-     * @covers                   Api\FileManagement::write
+     * @covers                   FileManagement::write
      */
     public function testWrite() : void
     {
         $filename = $this->getTestFilename();
         $expected = 'Example random number :' . rand(0, 15000);
 
-        $this->api->fileManagement()->write($filename, $expected);
-        $actual = $this->api->fileManagement()->read($filename);
-        $this->assertEquals($expected, $actual);
+        $this->api->write($filename, $expected);
+        $this->assertEquals($expected, $this->api->read($filename));
     }
 
     /**
      * @group internet
-     * @covers                   Api\FileManagement::update
+     * @covers                   FileManagement::update
      */
     public function testUpdate() : void
     {
         $filename = $this->getTestFilename();
         $expected = 'Example random number :' . rand(0, 15000);
 
-        $this->api->fileManagement()->update($filename, $expected);
-        $actual = $this->api->fileManagement()->read($filename);
+        $this->api->update($filename, $expected);
 
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $this->api->read($filename));
     }
 
     public function getTestFilename()
